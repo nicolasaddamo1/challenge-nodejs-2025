dd5b42de3f7c45b767c561f1dd2c68f3726e1de8
Nico <nicolasaddamo1@gmail.com>
2025-05-26 11:26:27 -0300

    feat: modified app module

--- challeng-olaclick/src/app.module.ts
+++ challeng-olaclick/src/app.module.ts
@@ -1,11 +1,44 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ScheduleModule } from '@nestjs/schedule';
+import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-redis-store';
+import { OrdersModule } from './orders/orders.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [OrdersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadModels: true,
+      synchronize: true,
+      define: {
+        paranoid: true,
+        timestamps: true,
+      },
+    }),
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: parseInt(process.env.REDIS_PORT || '6379', 10),
+      ttl: parseInt(process.env.CACHE_TTL || '60', 10),
+    }),
+    ScheduleModule.forRoot(),
+    OrdersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
