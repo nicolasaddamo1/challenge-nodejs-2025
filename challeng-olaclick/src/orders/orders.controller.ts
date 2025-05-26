89ef65ec47184edc63656e03f0b6dc392308d032
Nico <nicolasaddamo1@gmail.com>
2025-05-26 11:15:51 -0300

    feat: added controller

--- challeng-olaclick/src/orders/orders.controller.ts
+++ challeng-olaclick/src/orders/orders.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { CreateOrderDto } from './dto/create-orders.dto';
 
 @Controller('orders')
-export class OrdersController {}
+export class OrdersController {
+    constructor(private readonly ordersService: OrdersService) { }
+
+    @Get()
+    findAll() {
+        return this.ordersService.findAll();
+    }
+
+    @Post()
+    create(@Body() createOrderDto: CreateOrderDto) {
+        return this.ordersService.create(createOrderDto);
+    }
+}
